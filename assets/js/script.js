01f012dbab35b83750aac640d91ca071564ae305
max-las <lasserre.maxime@yahoo.fr>
2021-10-03 19:09:05 +0200

    progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,50 @@
 document.addEventListener("DOMContentLoaded", function(){
 	$(".flag").on("click", toggleFlag);
+
+	animateCSS("nav", "slideInDown");
+
+	if(document.querySelector(".main.home")){
+		animateCSS(".img-a", "fadeIn");
+		animateCSS(".left-title", "slideInLeft");
+		animateCSS(".short-text", "slideInRight");
+		animateCSS(".discover-button", "slideInRight");
+	}
+
+	barba.init({
+    transitions: [{
+      name: 'page-page',
+      to: {
+        custom: function(data){
+          return true;
+        }
+      },
+      leave(data) {
+        return gsap.to(data.current.container.querySelector(".barba-content"), {
+          transform: "translateY(-100%)"
+        });
+      },
+      enter(data) {
+        gsap.from(data.next.container.querySelector(".barba-content"), {
+          transform: "translateY(100%)"
+        });
+      }
+    }]
+  });
+});
+
+const animateCSS = (element, animation, prefix = 'animate__') => new Promise((resolve, reject) => {
+  const animationName = `${prefix}${animation}`;
+  const node = $(element);
+
+  node.addClass(`${prefix}animated ${animationName}`);
+
+  function handleAnimationEnd(event) {
+    event.stopPropagation();
+    node.removeClass(`${prefix}animated ${animationName}`);
+    resolve('Animation ended');
+  }
+
+  node.one('animationend', handleAnimationEnd);
 });
 
 function toggleFlag(){
--- content/photography.md
+++ content/photography.md
@@ -1,5 +1,5 @@
 ---
-title: "About"
+title: "Photographie | Maxime Lasserre"
 date: 2021-09-30T11:29:39+02:00
 draft: false
 ---
